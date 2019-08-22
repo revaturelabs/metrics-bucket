5890f3eac84ee9c0561641220fef97b5e6eff70d
loleron <f-gu@prolikewoah.com>
2019-08-22 10:41:22 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
 
   //domain for backend server
-  backend: "ec2-13-58-213-39.us-east-2.compute.amazonaws.com",
+  backend: "ec2-127-0-0-1.us-east-2.compute.amazonaws.com",
 
   //info for s3 bucket
   bucketName: "production",
